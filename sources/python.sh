ac532a1337651a92f191d3f5b3a0246eb2ef2163
alexrf45 <fonalex45@gmail.com>
2022-12-29 01:39:02 -0500

    spidy

--- sources/python.sh
+++ sources/python.sh
@@ -4,5 +4,6 @@ function python_tools() {
     cd /root/tools/ && git clone https://github.com/fleetcaptain/Turbolist3r.git && cd Turbolist3r/ && pip install -r requirements.txt
     cd /root/tools/ && git clone https://github.com/dievus/msdorkdump.git && cd msdorkdump/ && pip3 install -r requirements.txt
     cd /root/tools/ && git clone https://github.com/jordanpotti/CloudScraper && cd CloudScraper/ && pip install -r requirements.txt
+    cd /root/tools/ & git clone https://github.com/rivermont/spidy.git && cd spidy && pip install -r requirements.txt
 }
 python_tools
