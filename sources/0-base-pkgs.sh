3bd53824b16cbaa47143be81ccd7e225c8a93d36
alexrf45 <fonalex45@gmail.com>
2023-05-06 13:40:01 -0400

    added firefox

--- sources/0-base-pkgs.sh
+++ sources/0-base-pkgs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 function base() {
-    sudo apt-get install -y wget curl man git zsh \
-	tmux ruby ruby-dev vim nano p7zip-full kali-themes \
-	djvulibre-bin python3-pip python3-virtualenv libpcap-dev jq mousepad
+	sudo apt-get install -y wget curl man git zsh \
+		tmux ruby ruby-dev vim nano p7zip-full kali-themes \
+		djvulibre-bin python3-pip python3-virtualenv libpcap-dev jq mousepad \
+		firefox-esr
 }
 
 base
-
