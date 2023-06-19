def723bd28c37e832d88c922d0630e9be4187e20
alexrf45 <fonalex45@gmail.com>
2023-06-19 13:07:38 -0400

    added quick ctf recon script

---
+++ resources/recon.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+whatweb -a 1 -U=$AGENT -t 10 --wait=0.2 http://$IP/ >whatweb.txt
+
+mkdir $NAME-nmap/$NAME
+
+nmap -sC -sV -oA $NAME-nmap/$NAME -p- $IP
+
+ffuf -c -t 30 -p 0.1 -r -rate 100 -recursion -recursion-depth 3 \
+	-mc 200,302,403 -w /usr/share/seclists/Discovery/Web-Content/raft-small-words.txt \
+	-u http://$IP/FUZZ
+
+ffuf -c -t 10 -rate 100 -p 0.1 -o $NAME_domains.json -of json -H "Host:FUZZ.$DOMAIN" -mc 200,403 \
+	-w /usr/share/seclists/Discovery/DNS/subdomains-top1million-110000.txt -u http://$DOMAIN/
