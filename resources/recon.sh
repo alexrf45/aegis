fc880ed6e40a6e297fa5a0893257b2dd1e8289e9
alexrf45 <fonalex45@gmail.com>
2023-07-08 11:56:34 -0400

    testing ctf tmux env automation, sh script can't get domain automaticallly. removed subdomain ffuf cmd

--- resources/recon.sh
+++ resources/recon.sh
@@ -11,8 +11,3 @@ ffuf -c -t 30 -p 0.1 \
 	-H "User-Agent:$AGENT"
 -mc 200,302,403 -w /usr/share/seclists/Discovery/Web-Content/raft-small-words.txt \
 	-u http://$IP/FUZZ
-
-ffuf -c -t 10 -rate 100 -p 0.1 \
-	-H "User-Agent:$AGENT" \
-	-o $NAME_domains.json -of json -H "Host:FUZZ.$DOMAIN" -mc 200,403 \
-	-w /usr/share/seclists/Discovery/DNS/subdomains-top1million-110000.txt -u http://$DOMAIN/
