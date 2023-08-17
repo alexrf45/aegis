944187b0e7e724c1166a157c204702fe6470de35
alexrf45 <fonalex45@gmail.com>
2023-08-16 22:08:25 -0400

    tool version bump, new tmux theme is gruvbox

--- sources/1-tools.sh
+++ sources/1-tools.sh
@@ -3,10 +3,10 @@
 # Installs commonly used tools in the tools subdirectory of the kali home directory
 
 install_go() {
-	wget https://go.dev/dl/go1.20.6.linux-amd64.tar.gz &&
+	wget https://go.dev/dl/go1.21.0.linux-amd64.tar.gz &&
 		rm -rf /usr/local/go &&
-		sudo tar -C /usr/local -xzf go1.20.6.linux-amd64.tar.gz &&
-		rm go1.20.6.linux-amd64.tar.gz
+		sudo tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz &&
+		rm go1.21.0.linux-amd64.tar.gz
 }
 
 waybackurls_install() {
@@ -19,9 +19,9 @@ waybackurls_install() {
 }
 
 httpx_install() {
-	wget -q https://github.com/projectdiscovery/httpx/releases/download/v1.2.5/httpx_1.2.5_linux_amd64.zip &&
-		unzip httpx_1.2.5_linux_amd64.zip -d ./httpx &&
-		rm httpx_1.2.5_linux_amd64.zip &&
+	wget -q https://github.com/projectdiscovery/httpx/releases/download/v1.3.4/httpx_1.3.4_linux_amd64.zip &&
+		unzip httpx_1.3.4_linux_amd64.zip -d ./httpx &&
+		rm httpx_1.3.4_linux_amd64.zip &&
 		mv httpx/httpx /home/kali/.local/http-x &&
 		rm -r httpx/
 
@@ -44,9 +44,9 @@ active_directory() {
 		wget -q -O cme.zip \
 			"https://github.com/Porchetta-Industries/CrackMapExec/releases/download/v5.4.0/cme-ubuntu-latest-3.11.zip" &&
 		unzip cme.zip && chmod +x cme && sudo mv cme /home/kali/.local/cme && rm cme.zip &&
-		wget "https://github.com/fortra/impacket/releases/download/impacket_0_10_0/impacket-0.10.0.tar.gz" &&
-		gunzip impacket-0.10.0.tar.gz && tar -xvf impacket-0.10.0.tar &&
-		mv impacket-0.10.0/ /home/kali/.local/ && rm impacket-0.10.0.tar &&
+		wget "https://github.com/fortra/impacket/releases/download/impacket_0_11_0/impacket-0.11.0.tar.gz" &&
+		gunzip impacket-0.11.0.tar.gz && tar -xvf impacket-0.11.0.tar &&
+		mv impacket-0.11.0/ /home/kali/.local/ && rm impacket-0.11.0.tar &&
 		wget -q -O sharp.ps1 \
 			"https://github.com/BloodHoundAD/BloodHound/raw/master/Collectors/SharpHound.ps1" &&
 		wget -q -O SharpHound.exe \
@@ -66,9 +66,9 @@ pivot() {
 privesc() {
 	cd /home/kali/tools/ &&
 		wget -q -O linpeas.sh \
-			"https://github.com/carlospolop/PEASS-ng/releases/download/20230611-b11e87f7/linpeas_linux_amd64" &&
+			"https://github.com/carlospolop/PEASS-ng/releases/download/20230813-dc8384b3/linpeas_linux_amd64" &&
 		wget -q -O winpeas.exe \
-			"https://github.com/carlospolop/PEASS-ng/releases/download/20230611-b11e87f7/winPEASany.exe" &&
+			"https://github.com/carlospolop/PEASS-ng/releases/download/20230813-dc8384b3/winPEASany.exe" &&
 		wget -q -O pspy \
 			"https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64s"
 }
