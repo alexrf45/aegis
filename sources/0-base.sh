157500d3c3c3d21e670ca79bc792552099bb738f
alexrf45 <fonalex45@gmail.com>
2023-09-27 19:53:58 -0400

    added file binary

--- sources/0-base.sh
+++ sources/0-base.sh
@@ -2,7 +2,7 @@
 
 base() {
 	sudo apt-get install -y wget curl man git zsh \
-		tmux tree ruby ruby-dev vim nano p7zip-full kali-themes \
+		tmux tree file ruby ruby-dev vim nano p7zip-full kali-themes \
 		djvulibre-bin python3-pip python3-virtualenv libpcap-dev jq \
 		firefox-esr xpdf tmuxp man-db exploitdb
 }
