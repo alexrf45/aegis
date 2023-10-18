45f6ec828877c579f6577e60759261091b6488c7
alexrf45 <fonalex45@gmail.com>
2023-10-17 21:28:41 -0400

    added cmake, vinetto, csvtool

--- sources/0-base.sh
+++ sources/0-base.sh
@@ -4,7 +4,7 @@ base() {
 	sudo apt-get install -y wget curl man git zsh \
 		tmux tree mousepad hexcurse file ruby ruby-dev vim nano p7zip-full kali-themes \
 		djvulibre-bin python3-pip python3-virtualenv libpcap-dev jq \
-		firefox-esr xpdf tmuxp man-db exploitdb rpcbind nfs-common feh
+		firefox-esr xpdf tmuxp man-db exploitdb rpcbind nfs-common feh cmake
 }
 
 network() {
@@ -20,6 +20,12 @@ active_directory() {
 		powershell ldap-utils
 }
 
+misc_tools () {
+  sudo apt-get install -y \
+    csvtool vinetto sqlitebrowser
+}
+
 base
 network
 active_directory
+misc_tools
