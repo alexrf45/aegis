0f6c75fcd4de2f04c64eabc2cb8c393dccd8e625
alexrf45 <fonalex45@gmail.com>
2023-12-07 21:46:27 -0500

    added new tools

--- sources/0-base.sh
+++ sources/0-base.sh
@@ -4,28 +4,28 @@ base() {
 	sudo apt-get install -y wget curl man git zsh lolcat figlet \
 		tmux tree mousepad hexcurse file ruby ruby-dev vim nano p7zip-full kali-themes \
 		djvulibre-bin python3-pip python3-virtualenv libpcap-dev jq \
-		xpdf tmuxp man-db exploitdb rpcbind nfs-common feh cmake
+		xpdf tmuxp man-db exploitdb rpcbind nfs-common feh cmake recordmydesktop
 }
 
 network() {
 	sudo apt-get install -y netcat-traditional socat rlwrap nmap \
 		netdiscover masscan dnsutils onesixtyone braa tcpdump \
 		ftp telnet swaks snmpcheck snmpcheck snmp-mibs-downloader \
-		iputils-ping iproute2 proxychains sendmail ltrace
+		iputils-ping iproute2 proxychains sendmail ltrace raven
 }
 
 active_directory() {
 	sudo apt-get install -y \
 		smbclient smbmap evil-winrm bloodhound responder \
-		powershell ldap-utils 
-	}
+		powershell ldap-utils enum4linux-ng
+}
 
-misc_tools() {
+osint_tools() {
 	sudo apt-get install -y \
-		csvtool vinetto sqlitebrowser
+		csvtool vinetto sqlitebrowser exiflooter h8mail reconspider sn0int
 }
 
 base
 network
 active_directory
-misc_tools
+osint_tools
