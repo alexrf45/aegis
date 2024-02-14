33182ff8c783833c5d5194f1b345b56953a17230
alexrf45 <fonalex45@gmail.com>
2024-02-14 01:02:51 -0500

    git

--- sources/0-base.sh
+++ sources/0-base.sh
@@ -4,7 +4,7 @@ base() {
 	sudo apt-get install -y wget curl man git zsh lolcat figlet \
 		tmux tree mousepad hexcurse file ruby ruby-dev vim nano p7zip-full kali-themes \
 		djvulibre-bin python3-pip python3-virtualenv libpcap-dev jq \
-		xpdf tmuxp man-db exploitdb rpcbind nfs-common feh cmake recordmydesktop
+		git xpdf tmuxp man-db exploitdb rpcbind nfs-common feh cmake recordmydesktop
 }
 
 network() {
