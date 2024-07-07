0d41550cf8e0b8639c5a9a0796b5b881f7eca1ab
alexrf45 <fonalex45@gmail.com>
2024-07-07 18:31:34 -0400

    fzf stable, command history restored, zsh only, miniserve w/tls

--- sources/4-home.sh
+++ sources/4-home.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-mkdir -p $HOME/.config && mkdir -p $HOME/.zsh && mkdir -p $HOME/.bash &&
+mkdir -p $HOME/.config && mkdir -p $HOME/.zsh &&
   cp -r /home/kali/resources/tmux.conf $HOME/.tmux.conf &&
   cp -r /home/kali/resources/shell-upgrade.sh $HOME/.tools/shell-upgrade.sh &&
   cp -r /home/kali/resources/recon.sh $HOME/.local/bin/recon.sh && chmod +x $HOME/.local/bin/recon.sh &&
-  cp -r /home/kali/resources/proxychains.conf $HOME.proxychains/proxychains.conf &&
-  cp -r /home/kali/resources/kerbrute $HOME.local/bin/kerbrute && chmod +x /$HOME/.local/bin/kerbrute &&
+  cp -r /home/kali/resources/proxychains.conf $HOME/.proxychains/proxychains.conf &&
+  cp -r /home/kali/resources/kerbrute $HOME/.local/bin/kerbrute && chmod +x /$HOME/.local/bin/kerbrute &&
   cp -r /home/kali/resources/smbserver.py $HOME/.tools/smbserver.py &&
-  cp -r /home/kali/resources/bash/history $HOME/.history &&
+  cp -r /home/kali/resources/zsh/history $HOME/.history &&
   cp -r /home/kali/resources/starship.toml $HOME/.config/starship.toml
 
 wget -q "https://github.com/junegunn/fzf/releases/download/0.53.0/fzf-0.53.0-linux_amd64.tar.gz" -O fzf.tar.gz &&
@@ -24,16 +24,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 cp /home/kali/resources/zsh/.zshrc $HOME/.zshrc
 
 cp /home/kali/resources/zsh/kali.zsh-theme $HOME/.oh-my-zsh/custom/themes/kali.zsh-theme
-
-cp /home/kali/resources/bash/history $HOME/.commands
-
-cp /home/kali/resources/bash/.bashrc $HOME/.bashrc
-
-cp /home/kali/resources/bash/.bash_profile $HOME/.bash_profile
-
-cp /home/kali/resources/bash/aliases $HOME/.bash/aliases
-
-cp /home/kali/resources/bash/functions.sh $HOME/.bash/functions.sh
 
 cp /home/kali/resources/zsh/functions.sh $HOME/.zsh/functions.sh
 
